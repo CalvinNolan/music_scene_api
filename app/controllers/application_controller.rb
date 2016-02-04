3418cce98fa894a73e37c1ee4faf4c670991d551
Calvin Nolan <Calvinnolan@gmail.com>
2016-02-04 13:45:51 +0000

    Controller + Route changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-  	render text: "Group 34! Music Scene Playlist Generator API"
+  def index
+  	if(Playlist.find_by(location: "Dublin", genre: "Rock").blank?)
+  		generate_playlist
+  	else
+  		render json: Playlist.find_by(location: "Dublin", genre: "Rock").songs
+  	end
+  end
+
+  def generate_playlist
+  	render text: "You have no playlists!"
   end
 end
