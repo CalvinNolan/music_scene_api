022c8ff98091cd4ec48efcb9c60e0982eca9e290
Mathieu Molinengo <mathieu.moli@hotmail.com>
2016-03-06 21:06:13 +0000

    generate_playlist is okay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,84 +5,125 @@ class ApplicationController < ActionController::Base
   require 'open-uri'
   require 'httparty'
   require 'soundcloud'
-  @json
-  @genre
-  $area
-  @arrayOfArtist
+
+
   def index
-  		generate_playlist("Dublin","punk")
+    if(((params[:location] != "0") && (params[:location].to_i ==0))  && ((params[:genre] != "0") && (params[:genre].to_i ==0)))
+      location = params[:location]
+      genre = params[:genre]
+      if(Playlist.find_by(location: location, genre: genre).blank?)
+         generatePlaylist(location, genre)
+      else
+        render json: {:playlist => Playlist.find_by(location, genre), :songs => Playlist.find_by(location, genre).songs}
+      end
+    else 
+      render json: "Location and genre must be a string value"
+    end
   end
 
-  def generate_playlist(location,genre)
-    $area=location
+  def generate_playlist (location, genre)
+  end
+
+  def get_playlist_by_id
+    require 'uri'
+    
+    id_value = URI(request.original_url).path.split('/').last
+
+    # Code to check that the last segment of the url passed
+    # is of type int and to handle the case when it is not.
+    if (id_value == "0" || id_value.to_i != 0)
+      if (Playlist.find_by(id: id_value).blank?)
+        render json: "Playlist not found, please enter another id."
+      else
+        render json: {:playlist => Playlist.find_by(id: id_value), :songs => Playlist.find_by(id: id_value).songs}
+      end
+    else
+      render json: "Invalid request, id must be an Integer. Received " + id_value
+    end
+  end
+
+
+  def generatePlaylist(location,genre)
+
+    @location=location
     @genre=genre
 
 
-    @response = HTTParty.get('http://musicbrainz.org/ws/2/artist/?query=area:'+$area+'&fmt=json')
-    @json = JSON.parse(@response.body)
-
-    #@response = open('http://musicbrainz.org/ws/2/artist/?query=area:'+$area+'&fmt=json').read
-    #printf @response
-    #render json: @response
-    filterArtistsNames(@json)
-  end
-
-  def filterArtistsNames(responseJson)
-
+    @responseMusicBrainz = HTTParty.get('http://musicbrainz.org/ws/2/artist/?query=area:'+@location+'&fmt=json')
+    @jsonMB = JSON.parse(@responseMusicBrainz.body)
 
     @arrayOfArtist=Array.new
+    @arrayOfArtist=Array.new
+    @arrayOfSources=Array.new
+    @arrayOfSoundcloud=Array.new
+    @arrayOfAlltheSources=Array.new
+    @arrayOfUID=Array.new
+    @arrayOfTracks=Array.new
+    @user=Array.new
+    @track=Array.new
 
-    @decode=responseJson
 
-    @decode["artists"].each do |artist|
-      @arrayOfArtist.append(artist["name"])
+
+
+    @jsonMB["artists"].each do |artist|
+      @arrayOfArtist.append(artist["id"])
     end
 
-    getTracksbyArtists(@arrayOfArtist)
-
-  end
-
-
-  def getTracksbyArtists(arrayOfArtist)
-
-
-
-    # create a client object with your app credentials
-    client = Soundcloud.new(:client_id => '2b06757c21ec3bf6797de8804c0788ba')
-    @artistSound=Array.new
-
-    @tracks = client.get('/tracks', :genres => @genre).each do |track|
-      # now that we have the track id, we can get a list of comments, for example
-      @user=client.get("/users/#{track.user.id}")
-      @artistSound.append(@user.username)
+    for id in @arrayOfArtist
+      @responseOpenaura = HTTParty.get('http://api.openaura.com/v1/source/artists/'+id+'?id_type=musicbrainz%3Agid&api_key=')
+      @arrayOfAlltheSources.append(JSON.parse(@responseOpenaura.body))
     end
 
-    compareArtists(arrayOfArtist,@artistSound)
-
-    # find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
-    #@tracks = client.get('/tracks/123')#:user => {:username => "Blink 182"})
-
-    # a permalink to a track
-    #track_url = 'http://soundcloud.com/forss/voca-nomen-tuum'
-
-    # resolve track URL into track resource
-    #track = client.get('/resolve', :url => track_url)
-
-    #@comment=Array.new
-    # now that we have the track id, we can get a list of comments, for example
-    #client.get("/tracks/#{track.id}/comments").each do |comment|
-    #   @comment.append("Someone said: #{comment.body} at #{comment.timestamp}")
-    #end
-  end
-  def compareArtists(arrMusicBrainz,arrSound)
-
-    @commun=Array.new
-    arrMusicBrainz.each do |mb|
-      arrSound.each do |sd|
-        if(mb==sd)
-          @commun.append(mb)
-        end
+    for sources in @arrayOfAlltheSources
+      sources["sources"].each do |source|
+         @arrayOfSources.append(source)
       end
     end
+
+    for sources in @arrayOfSources
+      #for source in sources
+        if sources['oa_provider_id'] == 8
+          @arrayOfUID.append(sources["uid"])
+        end
+      #end
+    end
+
+    # create a client object with your app credentials
+    #client = Soundcloud.new(:client_id => '')
+    #@artistSound=Array.new
+
+    #@count=0;
+    newP=Playlist.create(location: @location, genre: @genre)
+    for uid in @arrayOfUID
+      @responseSound=HTTParty.get('http://api.soundcloud.com/users/'+uid+'/tracks?client_id=&genres='+@genre)
+      @tracks = JSON.parse(@responseSound.body)
+
+=begin
+      for track in @tracks
+        if(@artist==track["user_id"])
+          if(@count<1)
+            @count++
+            @arrayOfTracks.append(track)
+          end
+        else
+          @count=1
+        end
+
+
+        @artist=track["user_id"]
+=end
+        @track=@tracks[0]
+        @user=@track["user"]
+        @username=@user["username"]
+        @arrayOfTracks.append(@track)
+        new_song = Song.create(name:@track["title"],artist_name:@username,url:@track["permalink_url"],service:"Soundcloud")
+        PlaylistsSong.create(playlist_id: newP.id, song_id: new_song.id)
+      #end
+    end
+
+
+
+    #render json: @arrayOfTracks
+    #Playlist.find_by(location: @location, genre: @genre)
   end
 end
